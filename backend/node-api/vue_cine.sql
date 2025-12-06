9eb6d84a673c2ef2e86c33c1a3a8835fd7fd2e01
RaidenMGR47 <andres26palma@gmail.com>
2025-12-05 20:34:37 -0400

    Base de datos

--- backend/node-api/vue_cine.sql
+++ backend/node-api/vue_cine.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-12-2025 a las 02:39:47
+-- Tiempo de generación: 05-12-2025 a las 02:51:24
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
